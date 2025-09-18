b63b3fa2de0067be6732753a4867f73814b42e7f
KOHEI <ko.08financier.2mitchan4@gmail.com>
2025-09-18 10:49:34 +0900

    add: importまで

---
+++ frontend/src/components/freedrawingcomponent/index.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+import { useState, useRef } from "react";
+import { Stage, Layer, Line } from "react-konva";
+import IconButton from "@mui/material/IconButton";
+import { LuPencil, LuEraser } from "react-icons/lu";
